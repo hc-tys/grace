df60b81bb615a0e7aac60908887bc1dfbd49838c
hechao <hechao@xiaomi.com>
2017-06-23 13:39:42 +0800

    add set convert factory method for grace

--- core/src/main/java/grace/core/Grace.java
+++ core/src/main/java/grace/core/Grace.java
@@ -22,19 +22,21 @@ public class Grace {
 
     private static Executor sExecutor = GraceFactory.createStringSynExecutor();
 
+    private static Converter.Factory sConvertFactory = DEFAULT_CONVERTER_FACTORY;
+
     public static <S> S from(Class<S> service){
         return new GraceHttpServiceMapperImpl().toService(service);
     }
 
     public static GraceHttpCallMapper from(Request request){
-        return new GraceHttpCallMapperImpl(new MapperConfig.Builder<String>().executor(sExecutor).build(),request,DEFAULT_CONVERTER_FACTORY, GraceCall.CALL_FACTORY);
+        return new GraceHttpCallMapperImpl(new MapperConfig.Builder<String>().executor(sExecutor).build(),request,sConvertFactory, GraceCall.CALL_FACTORY);
     }
 
-    public static  GraceHttpCallMapper from(URL url){
+    public static GraceHttpCallMapper from(URL url){
         return from(Request.Builder.of(url).build());
     }
 
-    public static  GraceHttpCallMapper fromUrl(String url){
+    public static GraceHttpCallMapper fromUrl(String url){
         try {
             URL urlObj = new URL(url);
             return from(urlObj);
@@ -45,22 +47,26 @@ public class Grace {
     }
 
     public static GraceMapper from(byte[] value){
-        return new GraceMapperImpl(value,DEFAULT_CONVERTER_FACTORY);
+        return new GraceMapperImpl(value,sConvertFactory);
     }
 
     public static GraceMapper from(String value){
-        return new GraceMapperImpl(value, DEFAULT_CONVERTER_FACTORY);
+        return new GraceMapperImpl(value, sConvertFactory);
     }
 
     public static String toString(Object value){
-        return new GraceMapperImpl(value, DEFAULT_CONVERTER_FACTORY).to(String.class);
+        return new GraceMapperImpl(value, sConvertFactory).to(String.class);
     }
 
     public static GraceRuleMapper filter(String value){
-        return new GraceFilterMapperImpl(value,DEFAULT_CONVERTER_FACTORY);
+        return new GraceFilterMapperImpl(value,sConvertFactory);
     }
 
     public static void setDefaultExecutor(Executor executor){
         sExecutor = executor;
+    }
+
+    public static void setDefaultConverterFactory(Converter.Factory converterFactory){
+        sConvertFactory = converterFactory;
     }
 }
